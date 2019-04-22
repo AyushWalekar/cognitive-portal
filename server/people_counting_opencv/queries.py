e8940ab7382caf9f1ac4333213160903b6a36d06
AyushWalekar <walekarayush@gmail.com>
2019-04-22 17:03:05 +0530

    Modified queries to return bson dumps and accept datetime in string

--- server/people_counting_opencv/queries.py
+++ server/people_counting_opencv/queries.py
@@ -1,28 +1,13 @@
 from datetime import datetime, timedelta
 from app.ApplicationContext import ApplicationContext
+from bson.json_util import dumps
 
 app_context = ApplicationContext.getApplicationContext()
 app_props = app_context.get_props()
 
 
-def get_iso_date(Y, m, d, h=None, M=None, S=None):
-    """
-    Generic function to convert date tuple into iso format
-    :param Y: year
-    :param m: month
-    :param d: date
-    :param h: hour
-    :param M: minute
-    :param S: second
-    :return: date in ISO format
-    """
-    if S is None:
-        if M is None:
-            if h is None:
-                return datetime(Y, m, d).isoformat()
-            return datetime(Y, m, d, h).isoformat()
-        return datetime(Y, m, d, h, M).isoformat()
-    return datetime(Y, m, d, h, M, S).isoformat()
+def get_logs_str(from_time: str, to_time: str):
+    return get_logs(get_date_from_str(from_time), get_date_from_str(to_time))
 
 
 def get_logs(from_time: datetime, to_time: datetime = None):
@@ -40,7 +25,7 @@ def get_logs(from_time: datetime, to_time: datetime = None):
     collection = db_connector.collection
     print("Getting logs.. ", " From: ", from_time, " To: ", to_time)
     result_set = collection.find({"date": {"$gt": from_time, "$lt": to_time}})
-    return list(result_set)
+    return dumps(result_set)
 
 
 def get_logs_from_date_str(from_time: str, to_time: str = None):
@@ -50,6 +35,8 @@ def get_logs_from_date_str(from_time: str, to_time: str = None):
 
 
 def get_date_from_str(date_str):
+    if date_str is None:
+        return None
     # date_time_format_str = "%Y-%m-%dT%H:%M:%SZ"
     date_time_format_str = app_props["query"]["date_time_format_str"]
     print("Date time format is: ", date_time_format_str)
@@ -62,13 +49,27 @@ def get_todays_logs():
     return get_logs(from_time, curr_time)
 
 
-def get_that_days_logs(date):
+def get_that_days_logs(date_str: str):
+    date = get_date_from_str(date_str)
     from_time = date.replace(hour=0, minute=0, second=0)
     to_time = date.replace(hour=23, minute=59, second=59)
     return get_logs(from_time, to_time)
 
 
 def get_past_logs(days=0, hours=0, minutes=0, weeks=0, months=0, years=0):
+    if days is None:
+        days = 0
+    if hours is None:
+        hours = 0
+    if minutes is None:
+        minutes = 0
+    if weeks is None:
+        weeks = 0
+    if months is None:
+        months = 0
+    if years is None:
+        years = 0
+
     if years != 0:
         weeks += years * 52
     if months != 0:
@@ -81,9 +82,8 @@ def get_past_logs(days=0, hours=0, minutes=0, weeks=0, months=0, years=0):
         from_time = from_time.replace(hour=0, minute=0)
     return get_logs(from_time, curr_time)
 
-
 # result = get_todays_logs()
 # result = get_past_logs(months=1)
-result = get_logs_from_date_str("2019-01-01T01:01:01Z")
-# result = get_that_days_logs(datetime(2019, 3, 12))
-print(list(result).__len__())
+# result = get_logs_from_date_str("2019-01-01T01:01:01Z")
+# # result = get_that_days_logs(datetime(2019, 3, 12))
+# print(list(result).__len__())
